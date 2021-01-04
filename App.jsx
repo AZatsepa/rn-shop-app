9fdeef2b26887c9adae124b0bf8677a0a4e5b6d3
AZatsepa <artem.zatsepa@gmail.com>
2021-01-04 18:24:56 +0200

    Add store

--- App.jsx
+++ App.jsx
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import Colors from './constants/colors';
+import productReducer from './store/reducers/products';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,11 +16,19 @@ const styles = StyleSheet.create({
   },
 });
 
+const rootReducer = combineReducers({
+  product: productReducer,
+});
+
+const store = createStore(rootReducer);
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
